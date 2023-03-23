dfd18b3472e43d45e2a4f46a304b7dd967cf1b78
AndresAfar <90489682+AndresAfar@users.noreply.github.com>
2023-03-22 19:37:35 -0500

    Add files via upload

--- orddo-mainDev_1/orddo_dev.sql
+++ orddo-mainDev_1/orddo_dev.sql
@@ -1,63 +1,87 @@
 CREATE DATABASE orddo_dev;
-/*use DATABASE orddo_dev;*/
+use orddo_dev;
 
+CREATE TABLE rol(
+    id_rol int not null auto_increment,
+    nombreRol varchar(45),
+    primary key(id_rol),
+    unique(id_rol, nombreRol)
+);
 
-CREATE TABLE users(
-    idusers int not null auto_increment,
-    username varchar(45) not null,
-    password varchar(50) not null,
-    rol enum('administrador','empleado') not null,
-    primary key(idusers),
-    unique(username)
+CREATE TABLE usuario(
+    id_usuario int not null auto_increment,
+    rol_id_rol int not null,
+    documento varchar(30) not null,
+    usuario varchar(45) not null,
+    contrasena varchar(50) not null,
+    nombre varchar(45) not null,
+    apellido varchar(45) not null,
+    telefono varchar(15) not null,
+    fechaRegistroUser date not null,
+    primary key(id_usuario),
+    unique(usuario, documento),
+    foreign key(rol_id_rol) references rol(id_rol)
 )auto_increment=1;
 
-CREATE TABLE cargo(
-    document varchar(25) not null,
-    carName varchar(45) not null,
-    carLastName varchar(45) not null,
-    carPhone varchar(25) not null,
-    carDateRegis date not null,
-    primary key(document),
-    unique(document)
-)
+CREATE TABLE cliente(
+    id_cliente int not null auto_increment,
+    nombreCliente varchar(45) not null,
+    apellidoCliente varchar(45) not null,
+    primary key(id_cliente),
+    unique(id_cliente)
+);
 
 
-CREATE TABLE products(
-    idproducts int not null auto_increment,
-    proName varchar(45) not null,
-    proDescri text(150) not null,
-    proPrice double not null,
-    primary key(idproducts),
-    unique(proName)
+CREATE TABLE producto(
+    id_producto int not null auto_increment,
+    nombreProducto varchar(45) not null,
+    descripcionProducto text(150) not null,
+    precioProducto double not null,
+    estadoProducto varchar(20),
+    primary key(id_producto),
+    unique(nombreProducto, id_producto)
 )auto_increment=1;
 
 
 
-CREATE TABLE order_product(
-    order_idorder int not null,
-    product_idproduct int not null,
-    foreign key(order_idorder) references order(idorder),
-    foreign key(product_idproduct) references products(idproducts)
-)
+CREATE TABLE pedidos_producto(
+    pedido_id_pedido2 int not null,
+    pro_id_producto2 int not null
+);
 
-CREATE TABLE order(
-    idorder int not null auto_increment,
-    users_idusers int not null,
-    product_idproduct2 int not null,
-    nameClien varchar(45) not null,
-    lastnameClien varchar(45) not null,
-    totalCost double not null,
-    orDateRegis date not null,
-    primary key(idorder),
-    unique(idorder),
-    foreign key(users_idusers) references users(idusers),
-    foreign key(product_idproduct2) references order_product(product_idproduct)
+CREATE TABLE pedido(
+    id_pedido int not null auto_increment,
+    usu_id_usuario int not null,
+    pro_id_producto int not null,
+    cli_id_cliente int not null,
+    descripcionPedido text(150) not null,
+    estadoPedido varchar(20) not null,
+    totalPedido double not null,
+    fechaPedido date not null,
+    primary key(id_pedido),
+    unique(id_pedido)
 )auto_increment=1;
 
-CREATE TABLE report(
-    idreport int not null auto_increment,
-    order_idorder2 int not null,
-    nameReport varchar(45) not null,
-    reportDescri text(150) not null,
-    foreign key(order_idorder2) references order(idorder)
+CREATE TABLE reporte(
+    id_reporte int not null auto_increment,
+    pedido_id_pedido int not null,
+    nombreReporte varchar(45) not null,
+    descripcionReporte text(150) not null,
+    primary key(id_reporte),
+    unique(id_reporte)
 )auto_increment=1;
+
+
+#pedidos
+alter table orddo_dev.pedido add foreign key(usu_id_usuario) references usuario(id_usuario);
+alter table orddo_dev.pedido add foreign key(pro_id_producto) references pedidos_producto(pro_id_producto2);
+alter table orddo_dev.pedido add foreign key(cli_id_cliente) references cliente(id_cliente);
+
+
+
+#reporte
+alter table orddo_dev.reporte add foreign key(pedido_id_pedido) references pedido(id_pedido);
+
+#pedidos_producto
+alter table orddo_dev.pedidos_producto add foreign key(pedido_id_pedido2) references pedido(id_pedido);
+alter table orddo_dev.pedidos_producto add foreign key(pro_id_producto2) references producto(id_producto);
