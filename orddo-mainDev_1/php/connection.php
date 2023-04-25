3f6d5ce493f8df16078e093471010fcbc3a5eedf
AndresAfar <90489682+AndresAfar@users.noreply.github.com>
2023-04-24 20:57:39 -0500

    Add files via upload

--- orddo-mainDev_1/php/connection.php
+++ orddo-mainDev_1/php/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect("localhost", "root", "Andres 2003", "orddo_dev", "3307");
+$connection = mysqli_connect("localhost", "root", "", "orddo_dev", "3306");
 
 if ($connection->connect_error){
     die("No hay conexion: ".$connection->connect_error);
