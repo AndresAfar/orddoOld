6c86e1615d1bb50745108719c676b0f0a208bc7c
AndresAfar <90489682+AndresAfar@users.noreply.github.com>
2023-04-24 20:58:33 -0500

    Modificacion de consulta sql

--- orddo-mainDev_1/php/products/consulPro.php
+++ orddo-mainDev_1/php/products/consulPro.php
@@ -10,7 +10,7 @@ $result= $connection->query($sql);
 
 $dates = array('date' => array());
 
-
+//Cargar datos en tabla y para boton
 if($result -> num_rows>0){
     while($row=$result->fetch_array()){
         $dates['date'][]=array($row[0],$row[1],$row[2],$row[3],$row[4]);
