e2592f216d5251b64485b9258a4b675a7607095c
AndresAfar <90489682+AndresAfar@users.noreply.github.com>
2023-05-10 11:48:42 -0500

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect("localhost", "root", "Andres2003", "orddo_dev", "3306");
+$connection = mysqli_connect("localhost", "root", "", "orddo_dev", "3306");
 
 if ($connection->connect_error){
     die("No hay conexion: ".$connection->connect_error);
