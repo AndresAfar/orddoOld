39b7a6aebc34a47055e4b0c50318b771cb97c480
AndresAfar <90489682+AndresAfar@users.noreply.github.com>
2023-05-10 11:13:51 -0500

    Delete orddo-mainDev_1 directory

--- orddo-mainDev_1/js/tblShowAll.js
+++
@@ -1,1 +0,0 @@
-// cargar proudctos sin 
--- orddo-mainDev_1/php/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$connection = mysqli_connect("localhost", "root", "", "orddo_dev", "3306");
-
-if ($connection->connect_error){
-    die("No hay conexion: ".$connection->connect_error);
-}
-
-?>
