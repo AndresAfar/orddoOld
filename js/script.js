2693862de786698c075d49050d76492fc19426be
AndresAfar <90489682+AndresAfar@users.noreply.github.com>
2023-06-05 15:24:25 -0500

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-const upVal = document.querySelector('#input');
-const button = document.querySelector('#button');
-
-button.addEventListener('click',()=>{
-	if(upVal.value.length == 0){
-		alert("Los campos no pueden estar vacios")
-	}
-})
