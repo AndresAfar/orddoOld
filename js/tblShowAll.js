e37dddbf7f776261fad0d1424578d4a36a69fbf0
AndresAfar <90489682+AndresAfar@users.noreply.github.com>
2023-06-12 18:47:58 -0500

    Update tblShowAll.js
    Se agrega la funcion de buscar empleado loadSearchEmplo

--- js/tblShowAll.js
+++ js/tblShowAll.js
@@ -59,5 +59,57 @@ const loadSearchProduct = async()=>{
 }
 
 
+// Buscador empleados
 
-// cargar proudctos sin 
+const loadSearchEmplo = async()=>{
+
+    var nameEmplo = document.querySelector("#search").value;
+    var searchResult = ``;
+
+    if(nameEmplo===''){
+        Swal.fire({
+            position: 'top-end',
+            icon: 'error',
+            title: 'El campo esta vacio',
+            showConfirmButton: false,
+            timer: 1300
+          })
+    }else{
+
+        const data = new FormData();
+        data.append("nameEmplo",nameEmplo);
+
+        var res = await fetch("php/employe/searchEmplo.php", {
+            method:'POST',
+            body: data
+        });
+
+        var shoEm = await res.json();
+        
+        if(shoEm.data.length === 0){
+            Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'No se ha encontrado ningun empleado',
+                showConfirmButton: false,
+                timer: 1300
+              })
+        }else{
+
+            shoEm.data.forEach(item => {
+                searchResult +=`
+                    <tr>
+                        <td>${item[2]}</td>
+                        <td>${item[5]}</td>
+                        <td>${item[6]}</td>
+                        <td>${item[7]}</td>
+                        <td><button class="btn btn-warning m-1" style="height: 30px; width: 60px; font-size: 12px;" data-bs-toggle="modal" data-bs-target="#editModalEmplo" onclick="editEmplo(${item[0]})">Editar</button><button class="btn btn-danger" style="height: 30px; width: 60px; font-size: 12px;" onclick="confirmDelete(${item[0]})">Eliminar</button></td>
+                    </tr>
+                `;
+            });
+            document.querySelector("#showSearch").innerHTML=searchResult;
+            document.getElementById("consult-result").style.visibility = 'visible';
+            document.getElementById("div-result").style.visibility = 'visible';
+        }
+    }
+}
